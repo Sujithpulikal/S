563ca472bbabcb7a8b387a27f3d893830c7bd928
Sujith C P <76515895+Sujithpulikal@users.noreply.github.com>
2025-03-13 15:46:33 +0530

    Update S.py

--- S.py
+++ S.py
@@ -1,14 +1,1 @@
-for accommodation in accommodation_list:
-        data = (
-            request_id,
-            accommodation.location,
-            accommodation.hotel_name,
-            accommodation.check_in,
-            accommodation.check_out,
-            accommodation.accommodation_cost,
-            accommodation.food_cost,
-            accommodation.total_accommodation_cost,
-            user_id,
-            user_id
-        )
-        cursor.execute(query, data)
+
