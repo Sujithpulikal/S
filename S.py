d41eea2b56788ed0002a20db4f97f62549a94d94
Sujith C P <76515895+Sujithpulikal@users.noreply.github.com>
2025-03-13 14:30:17 +0530

    Update S.py

--- S.py
+++ S.py
@@ -1,6 +1,39 @@
-cursor.execute("""
-                    INSERT INTO expense_attachments (expense_request_id, file_name, file_size, file_data, file_type,
-                                                     created_by, created_on, modified_by, modified_on)
-                    VALUES (%s, %s, %s, %s, %s, %s, NOW(), %s, NOW())
-                """, (expense_request_id, attachment.file_name, attachment.file_size,
-                      attachment.file_data, attachment.file_type, request.user_id, request.user_id))
+query = """
+INSERT INTO expense_travel_mapper (
+    expense_request_id,
+    expense_travel_type_id,
+    travel_start_date,
+    travel_end_date,
+    travel_origin,
+    travel_destination,
+    note,
+    travel_total_expense,
+    created_by,
+    created_on,
+    modified_by,
+    modified_on
+) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, NOW(), %s, NOW())
+"""
+
+# Sample Data Insertion
+def add_travel_mapper(conn, request: ExpenseRequest):
+    cursor = conn.cursor()
+
+    if request.Travel:
+        for travel in request.Travel:
+            data = (
+                request.expense_request_id,
+                travel.expense_travel_type_id,
+                travel.travel_start_date,
+                travel.travel_end_date,
+                travel.travel_origin,
+                travel.travel_destination,
+                travel.note,
+                travel.travel_total_expense,
+                request.user_id,
+                request.user_id
+            )
+            cursor.execute(query, data)
+
+    conn.commit()
+    cursor.close()
